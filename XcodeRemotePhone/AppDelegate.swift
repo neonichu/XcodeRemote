a44fa761b08fbe97cdd7fa2e9b21cc528b02f491
Boris Bügling <boris@icculus.org>
2015-05-03 18:22:45 +0200

    Added phone and watch targets.

---
+++ XcodeRemotePhone/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  XcodeRemotePhone
+//
+//  Created by Boris Bügling on 03/05/15.
+//  Copyright (c) 2015 Boris Bügling. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+}
